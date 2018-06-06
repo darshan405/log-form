6f09992d72430390888a4b7972ac6836cfad559f
darshan405 <dshivakumar@qwinix.io>
2018-06-06 18:36:47 +0530

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { FeedPageComponent } from './feed-page/feed-page.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'dashboard-page', pathMatch: 'full' },
+  { path: 'dashboard-page', component: DashboardPageComponent },
+  { path: 'feed-page', component: FeedPageComponent }
+  // { path: 'new', component: NewComponent },
+  // { path: '**', component: PageNotFoundComponent }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardPageComponent,
+    FeedPageComponent
+    
   ],
   imports: [
+    RouterModule,
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/feed-page/feed-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-feed-page',
+  templateUrl: './feed-page.component.html',
+  styleUrls: ['./feed-page.component.css']
+})
+export class FeedPageComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+}
